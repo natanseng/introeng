bba867e2dbd192afc607726db50f0493dab7ff18
Natan Santos <natan.seng@hotmail.com>
2019-11-20 11:39:33 -0300

    Add read option

---
+++ learn_audio.py
@@ -0,0 +1,8 @@
+import wave
+obj = wave.open('audio.wav','r')
+print( "Number of channels",obj.getnchannels())
+print ( "Sample width",obj.getsampwidth())
+print ( "Frame rate.",obj.getframerate())
+print ("Number of frames",obj.getnframes())
+print ( "parameters:",obj.getparams())
+obj.close()
