4f4058dafbbe5397cefa4cfe2ac392e2c123707a
Natan Santos <natan.seng@hotmail.com>
2019-11-20 10:35:13 -0300

    Add comment

--- começando.py
+++ começando.py
@@ -1,10 +1,18 @@
+#Importando pacotes ( import (pacote) como a sigla (YYY) )
+
+
 import numpy as np  
 import matplotlib.pyplot as plt 
 import soundfile as sf 
 
+#Lendo amostra
+
 signal, samplerate = sf.read('audio.wav')
+
+#Definindo como será o gráfico
 
 time = np.arange(0, len(signal) * 1/samplerate, 1/samplerate )
 
+#Exibindo gráfico 
 plt.plot(time,signal)
 plt.show()
