dcb33bdd2894098b96cb633029299a4b6f8a3ed4
Natan <natan.seng@hotmail.com>
2019-11-27 11:02:14 -0300

    Interface learn_audio.py

--- learn_audio.py
+++ learn_audio.py
@@ -1,8 +1,21 @@
 import wave
 obj = wave.open('audio.wav','r')
-print( "Number of channels",obj.getnchannels())
-print ( "Sample width",obj.getsampwidth())
-print ( "Frame rate.",obj.getframerate())
-print ("Number of frames",obj.getnframes())
-print ( "parameters:",obj.getparams())
+
+print("INTRODUÇÃO À ENGENHARIA DE COMPUTAÇÃO")
+
+print("BEM VINDO AO IDENTIFICADOR DE ÁUDIO!")
+
+escolha_do_usuário=int(input("Pressione para exibir: 1 para Número de Canais, 2 para Largura de Amostragem, 3 para Taxa Periódica de Quadros, 4 para Número de Quadros: "))
+
+if escolha_do_usuário == 1:
+    print( "Número de Canais: ",obj.getnchannels())
+
+elif escolha_do_usuário == 2: 
+    print ( "Largura de Amostragem: ",obj.getsampwidth())
+
+elif escolha_do_usuário == 3:
+    print ( "Taxa Periódica de Quadros: ",obj.getframerate())
+
+elif escolha_do_usuário == 4:
+    print ("Número de Quadros: ",obj.getnframes())
 obj.close()
